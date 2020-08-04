d09099e9ff4572bf09d208efcb7accf0b7a42e00
maureenchepkirui <maureenchepkirui005@gmail.com>
2020-08-04 19:37:27 +0300

    Implement ListView and Adapter

--- app/src/main/java/com/maureenbett/bookhub/libraryActivity.java
+++ app/src/main/java/com/maureenbett/bookhub/libraryActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class libraryActivity extends AppCompatActivity {
+    public String[] novels = new String[] {"Jane Eyre","Wuthering Heights","Moby Dick","Pride and Prejudice","The Great Gatsby ","Frankenstein","Robison Crusoe","Little Women","Crime and Punishment","The Notebook","The Next Always","The Kiss","The Sweetest Thing ","A Second Chance"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
