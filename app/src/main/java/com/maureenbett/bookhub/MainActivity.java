ad14db32162e9402669480e2a88a139b36f8596c
maureenchepkirui <maureenchepkirui005@gmail.com>
2020-08-04 19:14:54 +0300

    Make the library button create a new intent that takes the user from the main activity to library activity

--- app/src/main/java/com/maureenbett/bookhub/MainActivity.java
+++ app/src/main/java/com/maureenbett/bookhub/MainActivity.java
@@ -2,13 +2,25 @@ package com.maureenbett.bookhub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mLibraryButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mLibraryButton = (Button) findViewById(R.id.libraryButton);
+        mLibraryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, libraryActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
