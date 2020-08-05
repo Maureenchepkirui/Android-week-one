6c0fa774b89a91c06ab1861d14c305746674b59e
maureenchepkirui <maureenchepkirui005@gmail.com>
2020-08-05 12:13:39 +0300

    Make the reading list button navigate to a new activity

--- app/src/main/java/com/maureenbett/bookhub/GenresActivity.java
+++ app/src/main/java/com/maureenbett/bookhub/GenresActivity.java
@@ -2,11 +2,15 @@ package com.maureenbett.bookhub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class GenresActivity extends AppCompatActivity {
+    private Button mreadingListButton;
     private ListView mListView;
     public String[] genres = new String[]{"classic","crime","epic","fable","fairytale","folktale","gothic fiction",
             "historical fiction","horror","thriller","romance","action","humor","legend","magical realism","meta fiction","mythology",
@@ -21,6 +25,15 @@ public class GenresActivity extends AppCompatActivity {
 
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, genres);
         mListView.setAdapter(adapter);
-
+        Intent intent = getIntent();
+        mreadingListButton = (Button) findViewById(R.id.readingListButton);
+        mreadingListButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(GenresActivity.this, GenresActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
